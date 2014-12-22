8eece044b872a1358ad7e6190b96be764acd13af
Atul Varma <varmaa@gmail.com>
2014-12-22 11:57:05 -0500

    Origination.

---
+++ require-config.js
@@ -0,0 +1,19 @@
+var require = {
+  paths: {
+    "firebase": "vendor/firebase",
+    "text": "vendor/require.text",
+    "jsx": "vendor/require.jsx",
+    "JSXTransformer": "vendor/react/build/JSXTransformer",
+    "react": "vendor/react/build/react-with-addons"
+  },
+  jsx: {
+    fileExtension: '.jsx'
+  },
+  shim: {
+    "firebase": {
+      exports: "Firebase"
+    }
+  },
+  // Grrr. http://stackoverflow.com/a/8479953
+  urlArgs: "bust=" + Date.now()
+};
---
+++ src/app.jsx
@@ -0,0 +1,42 @@
+define(function(require) {
+  var React = require('react');
+
+  var Item = React.createClass({
+    handleClick: function() {
+      this.props.firebaseRef.set(this.props.item + 1);
+    },
+    render: function() {
+      return <button onClick={this.handleClick}>{this.props.item}</button>;
+    }
+  });
+
+  var App = React.createClass({
+    getInitialState: function() {
+      return {
+        items: null
+      };
+    },
+    componentWillMount: function() {
+      this.props.firebaseRef.on("value", this.handleFirebaseRefValue);
+    },
+    componentWillUnmount: function() {
+      this.props.firebaseRef.off("value", this.handleFirebaseRefValue);
+    },
+    handleFirebaseRefValue: function(snapshot) {
+      this.setState({items: snapshot.val()});
+    },
+    render: function() {
+      var items = this.state.items || {};
+      var itemsRef = this.props.firebaseRef;
+      return (
+        <div>
+          {Object.keys(items).map(function(key) {
+            return <Item key={key} item={items[key]} firebaseRef={itemsRef.child(key)}/>;
+          })}
+        </div>
+      );
+    }
+  });
+
+  return App;
+});
