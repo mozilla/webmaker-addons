ff0d3769a78a0e3f160c774c5af75482b0fc4c32
Atul Varma <varmaa@gmail.com>
2014-12-25 16:53:39 -0500

    fixed hilarious readability/bug in bin/make-font-list.js.

--- bin/make-font-list.js
+++ bin/make-font-list.js
@@ -7,10 +7,10 @@ var URL = "https://www.googleapis.com/webfonts/v1/webfonts?key=" +
 var ROOT_DIR = __dirname + '/..';
 var FILENAME = "src/fonts/font-list.json";
 
-request(URL, function(err, body) {
+request(URL, function(err, res, body) {
   if (err) throw err;
   var fonts = {};
-  var fontList = JSON.parse(body.body).items.map(function(font) {
+  var fontList = JSON.parse(body).items.map(function(font) {
     return font.family;
   });
   fs.writeFileSync(ROOT_DIR + '/' + FILENAME, JSON.stringify(fontList));
