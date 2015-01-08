2b9b0c6098e155a04475561e2723d11970d2f7a9
Atul Varma <varmaa@gmail.com>
2015-01-08 12:27:01 -0500

    use htmlshot at cupcakes.hivelearningnetworks.org:3000.

--- src/export-modal.jsx
+++ src/export-modal.jsx
@@ -23,7 +23,7 @@ define(function(require) {
     },
     handleExportToPNG: function(e) {
       e.preventDefault();
-      window.open('http://localhost:3000/shot?html=' +
+      window.open('http://cupcakes.hivelearningnetworks.org:3000/shot?html=' +
                   encodeURIComponent(this.props.html));
     },
     createDataURL: function(html) {
