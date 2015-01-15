43de02c3b3042e12216ac79d11e11db28688800a
Atul Varma <varmaa@gmail.com>
2015-01-15 08:42:40 -0500

    refactor selection-toolbar.jsx a bit.

--- src/selection-toolbar.jsx
+++ src/selection-toolbar.jsx
@@ -1,29 +1,33 @@
 define(function(require) {
+  var _ = require('underscore');
   var React = require('react');
   var TypeMap = require('./type-map');
   var BaseSelectionActions = require('jsx!./base-selection-actions');
 
   var SelectionToolbar = React.createClass({
+    createAction: function(actionClass) {
+      var selectedItem = this.getSelectedItem();
+      var selectedItemFirebaseRef = this.props.firebaseRef
+        .child(this.props.selectedItem).child('props');
+      return React.createElement(
+        actionClass,
+        _.extend({
+          itemType: selectedItem.type
+        }, TypeMap[selectedItem.type].DEFAULT_PROPS, selectedItem.props, {
+          allItems: this.props.items,
+          firebaseRef: selectedItemFirebaseRef
+        })
+      );
+    },
+    getActionClasses: function() {
+      return [BaseSelectionActions]
+        .concat(TypeMap[this.getSelectedItem().type].SelectionActions || []);
+    },
+    getSelectedItem: function() {
+      return this.props.items[this.props.selectedItem];
+    },
     render: function() {
       if (!this.props.selectedItem) return null;
-
-      var key = this.props.selectedItem;
-      var firebaseRef = this.props.firebaseRef.child(key).child('props');
-      var item = this.props.items[key];
-      var actions = [BaseSelectionActions]
-        .concat(TypeMap[item.type].SelectionActions || [])
-        .map(function(componentClass, i) {
-          var component = React.createElement(
-            componentClass,
-            _.extend({
-              itemType: item.type
-            }, TypeMap[item.type].DEFAULT_PROPS, item.props, {
-              allItems: this.props.items,
-              firebaseRef: firebaseRef
-            })
-          );
-          return <li key={i}>{component}</li>;
-        }, this);
 
       return (
         <div className="container" style={{
@@ -31,7 +35,11 @@ define(function(require) {
           bottom: 0,
           left: 0
         }}>
-          <ul className="list-inline">{actions}</ul>
+          <ul className="list-inline">
+            {this.getActionClasses().map(function(actionClass, i) {
+               return <li key={i}>{this.createAction(actionClass)}</li>;
+            }, this)}
+          </ul>
         </div>
       );
     }
