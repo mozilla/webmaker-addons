37b4ae8b482e662dec309a18d711192c9d203bd3
Atul Varma <varmaa@gmail.com>
2015-02-10 15:28:37 -0500

    added addon/build-xpi.sh.

---
+++ addon/build-xpi.sh
@@ -0,0 +1,7 @@
+#! /usr/bin/bash
+
+cfx xpi \
+  --update-link \
+  https://github.com/xmatthewx/webmaker-addons/addon/webmaker-addon.xpi \
+  --update-url \
+  https://github.com/xmatthewx/webmaker-addons/addon/webmaker-addon.update.rdf
