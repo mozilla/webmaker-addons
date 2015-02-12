37e273c043d44bdfb7aed21a3d61253de4cd7eb1
Atul Varma <varmaa@gmail.com>
2015-02-12 17:17:48 -0500

    add embedAPI.animateImageIntoCanvas().

--- src/embed-api.js
+++ src/embed-api.js
@@ -7,6 +7,39 @@ define(function(require) {
         .refs['add-image-button'];
 
       var embedAPI = {
+        animateImageIntoCanvas: function(options) {
+          var destRect = this.getImagePlacement(options.naturalWidth,
+                                                options.naturalHeight);
+          var img = document.createElement('img');
+          img.setAttribute('src', options.src);
+          img.style.position = 'absolute';
+          ['transition',
+           'mozTransition',
+           'webkitTransition'].forEach(function(prop) {
+            if (prop in img.style)
+              img.style[prop] = 'all ' + (options.duration / 1000) + 's';
+          });
+          img.style.top = options.top + 'px';
+          img.style.left = options.left + 'px';
+          img.style.width = options.width + 'px';
+          img.style.height = options.height + 'px';
+          document.body.appendChild(img);
+
+          /* Force reflow, so the browser knows bust a CSS transition.
+           * For more information, see http://stackoverflow.com/a/21665117. */
+          img.offsetHeight;
+
+          img.style.top = destRect.top + 'px';
+          img.style.left = destRect.left + 'px';
+          img.style.width = destRect.width + 'px';
+          img.style.height = destRect.height + 'px';
+
+          setTimeout(function() {
+            this.addImage(options.src, function() {
+              img.parentNode.removeChild(img);
+            });
+          }.bind(this), options.duration);
+        },
         getImagePlacement: function(width, height) {
           var scale = addImageButton.scaleToFit(width, height);
           var pointerScale = app.getPointerScale();
