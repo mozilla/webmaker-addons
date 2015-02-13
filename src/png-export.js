7f9fdfc84e23db104c66ff30e3f4c490f1c448ed
Atul Varma <varmaa@gmail.com>
2015-02-13 08:51:43 -0500

    added src/png-export.js.

---
+++ src/png-export.js
@@ -0,0 +1,55 @@
+define(function(require) {
+  var base64 = require('./base64');
+  var PNGBaker = require('png-baker');
+
+  var PNGExport = {
+    htmlToDataURL: function(html) {
+      var utf8 = base64.strToUTF8Arr(html);
+      return 'data:text/html;charset=utf-8;base64,' +
+             base64.base64EncArr(utf8);
+    },
+    jsonToDataURL: function(obj) {
+      var utf8 = base64.strToUTF8Arr(JSON.stringify(obj));
+      return 'data:application/json;charset=utf-8;base64,' +
+             base64.base64EncArr(utf8);
+    },
+    jsonFromDataURL: function(url) {
+      var match = url.match(
+        /^data:application\/json;charset=utf-8;base64,(.+)/
+      );
+
+      try {
+        var json = base64.UTF8ArrToStr(base64.base64DecToArr(match[1]));
+        return JSON.parse(json);
+      } catch (e) {
+        return null;
+      }
+    },
+    export: function(options, cb) {
+      var url = window.BASE_HTMLSHOT_URL + 'shot';
+      var html = options.html;
+      var req = new XMLHttpRequest();
+      var itemsURL = this.jsonToDataURL(options.items);
+      var htmlURL = this.htmlToDataURL(html);
+
+      req.open('POST', url);
+      req.responseType = 'arraybuffer';
+      req.setRequestHeader("Content-type",
+                           "application/x-www-form-urlencoded");
+      req.onload = function() {
+        if (req.status != 200)
+          return cb(new Error("XMLHttpRequest got HTTP " + req.status));
+        var baker = new PNGBaker(req.response);
+        baker.textChunks['webmaker-addon:items-url'] = itemsURL;
+        baker.textChunks['webmaker-addon:html-url'] = htmlURL;
+        cb(null, URL.createObjectURL(baker.toBlob()));
+      };
+      req.onerror = function() {
+        cb(new Error("XMLHttpRequest failed"));
+      };
+      req.send('html=' + encodeURIComponent(html));
+    }
+  };
+
+  return PNGExport;
+});
